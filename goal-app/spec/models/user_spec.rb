8d5c90e2c3bb051c191580dc02418d9d62ff39fe
Shan Kim <shankim35@gmail.com>
2023-02-22 14:59:07 -0500

    made users model

---
+++ goal-app/spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:session_token) }
+
+end
